8676e586668ee88c04ffd1ec0ca13e6c2af0dca0
ShayFae <ifrahnoor108@gmail.com>
2022-01-24 21:55:50 -0500

    Update

--- express_server.js
+++ express_server.js
@@ -7,9 +7,9 @@ app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-function generateRandomString() {
+const generateRandomString = function() {
   //Num to string then return from 2 the length of 6(no decimal!)
-  Math.random().toString(36).substr(2, 6)
+  return Math.random().toString(36).substr(2, 6);
 };
 
 const urlDatabase = {
@@ -47,8 +47,12 @@ app.get("/urls", (req, res) => {
 
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  console.log(req.body);
+  // res.send("Ok");
+  const randomString = generateRandomString();
+  urlDatabase[randomString] = req.body.longURL;
+
+  res.redirect("/urls/:shortURL");
 });
 
 app.get("/urls/new", (req, res) => {
@@ -56,8 +60,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  // for(const value in urlDatabase) {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
-  // }
+  // res.redirect(longURL);
 });
